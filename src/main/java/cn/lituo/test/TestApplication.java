2b03638d216f23de56c3224d6c35278d798fa97e
cuiyang <452871720@qq.com>
2019-05-30 13:43:04 +0800

    tijiaowenjian

---
+++ src/main/java/cn/lituo/test/TestApplication.java
@@ -0,0 +1,25 @@
+package cn.lituo.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author cuiyang
+ * @Date 2019/5/30 13:34
+ * @Version 1.0.0
+ */
+@SpringBootApplication
+@RestController
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class,args);
+    }
+
+    @RequestMapping("/aaa")
+    public String index(){
+        return "cuyang";
+    }
+}
